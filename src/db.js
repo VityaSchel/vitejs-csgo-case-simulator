bf4aafae108c4a0b55c24f40921ebb3ea99f2d7f
Viktor <vityaschel@utidteam.com>
2022-01-06 19:54:20 +0400

    build

--- src/db.js
+++
@@ -1,9 +0,0 @@
-import Dexie from 'dexie'
-
-const db = new Dexie('csdrop')
-db.version(1).stores({
-  items: ['++id', 'name', 'ownerID'].join(', '),
-  users: ['++id', 'name', 'balance'].join(', '),
-})
-
-export default db
